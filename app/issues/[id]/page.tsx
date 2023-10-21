81433b7138f2e9a489464dffbad575e1aa6cc0b8
BokaiWang <bk.wang1989@gmail.com>
2023-10-21 09:28:57 -0400

    Refactor - apply single responsibility principle

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,11 +1,9 @@
-import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
-import { Button, Card, Flex, Grid, Heading, Text, Box } from "@radix-ui/themes";
+import { Box, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
-import React, { FC } from "react";
-import Markdown from "react-markdown";
-import { Pencil2Icon } from "@radix-ui/react-icons";
-import Link from "next/link";
+import { FC } from "react";
+import EditButton from "./EditButton";
+import IssueDetails from "./IssueDetails";
 
 interface Props {
   params: { id: string };
@@ -22,20 +20,10 @@ const IssueDetailsPage: FC<Props> = async ({ params }) => {
     return (
       <Grid columns={{ initial: "1", md: "2" }} gap="3">
         <Box>
-          <Heading>{issue.title}</Heading>
-          <Flex my="2" gap="3">
-            <IssueStatusBadge status={issue.status} />
-            <Text>{issue.createdAt.toDateString()}</Text>
-          </Flex>
-          <Card className="prose" mt="4">
-            <Markdown>{issue.description}</Markdown>
-          </Card>
+          <IssueDetails issue={issue} />
         </Box>
         <Box>
-          <Button>
-            <Pencil2Icon />
-            <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
-          </Button>
+          <EditButton issueId={issue.id} />
         </Box>
       </Grid>
     );
