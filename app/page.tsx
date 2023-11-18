750f7689c817ee217c7374aa101c4fea69c9b36e
BokaiWang <bk.wang1989@gmail.com>
2023-11-18 09:47:05 -0500

    Build LatestIssues component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
-import { FC } from "react";
-import Pagination from "./components/Pagination";
-interface Props {
-  searchParams: { page: string };
-}
-export default function Home({ searchParams }: Props) {
-  return (
-    <Pagination
-      itemCount={22}
-      pageSize={10}
-      currentPage={parseInt(searchParams.page)}
-    />
-  );
+import LatestIssues from "./LatestIssues";
+
+export default async function Home() {
+  return <LatestIssues />;
 }
